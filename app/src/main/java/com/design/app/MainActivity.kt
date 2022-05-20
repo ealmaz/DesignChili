69e467017003a2364436c4a2bd0c2b294aedfec9
ealmaz <ealmaz@nurtelecom.kg>
2022-05-20 16:17:22 +0600

    DOM-408 -> refactor button styles

--- app/src/main/java/com/design/app/MainActivity.kt
+++ app/src/main/java/com/design/app/MainActivity.kt
@@ -2,10 +2,13 @@ package com.design.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
         setContentView(R.layout.activity_main)
     }
 }
