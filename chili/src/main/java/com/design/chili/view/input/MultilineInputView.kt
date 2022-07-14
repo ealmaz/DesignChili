d3a770649815b91c077d74b8b1404ad849d17cda
Kamila Ulanbekova <kulanbekova@nurtelecom.kg>
2022-07-14 12:21:54 +0600

    Add MultilineInputView and MaxLinesTextWatcher

---
+++ chili/src/main/java/com/design/chili/view/input/MultilineInputView.kt
@@ -0,0 +1,51 @@
+package com.design.chili.view.input
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import com.design.chili.R
+import com.design.chili.view.input.text_watchers.MaxLinesTextWatcher
+
+class MultilineInputView : BaseInputView {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        obtainAttributes(attrs)
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        obtainAttributes(attrs)
+    }
+
+    private fun obtainAttributes(attrs: AttributeSet) {
+        context?.obtainStyledAttributes(attrs, R.styleable.MultilineInputView, R.attr.multilineInputDefaultStyle, R.style.InputViewStyle_Simple)?.run {
+            getInteger(R.styleable.MultilineInputView_maxLength, -1)
+                .takeIf { it != -1 }?.let { setMaxLength(it) }
+            getInteger(R.styleable.MultilineInputView_maxLines, -1)
+                .takeIf { it != -1 }?.let { setMaxLines(it) }
+            getInteger(R.styleable.MultilineInputView_minLines, -1)
+                .takeIf { it != -1 }?.let { setMinLines(it) }
+
+            setupViews()
+            recycle()
+        }
+    }
+
+    private fun setupViews() {
+        setGravity(Gravity.LEFT)
+        hideAction()
+        hideMessage()
+        enableMultiline()
+    }
+
+    private fun enableMultiline() {
+        view.inputField.isSingleLine = false
+    }
+
+    private fun setMaxLines(charCount: Int) {
+        view.inputField.addTextChangedListener(MaxLinesTextWatcher(view.inputField, charCount))
+    }
+
+    private fun setMinLines(charCount: Int) {
+        view.inputField.minLines = charCount
+    }
+}
