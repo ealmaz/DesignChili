bbd59fcd2a2136a8f80a754e6b9b711440fcceb8
ealmaz <ealmaz@nurtelecom.kg>
2022-07-27 10:53:09 +0600

    refactor ToggleCellView

--- chili/src/main/java/com/design/chili/view/cells/ToggleCellView.kt
+++ chili/src/main/java/com/design/chili/view/cells/ToggleCellView.kt
@@ -10,15 +10,19 @@ import com.design.chili.R
 open class ToggleCellView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.cellViewDefaultStyle,
-    defStyleRes: Int = R.style.Chili_CellViewStyle_BaseCellViewStyle
+    defStyleAttr: Int = R.attr.toggleCellViewDefaultStyle,
+    defStyleRes: Int = R.style.Chili_CellViewStyle_BaseCellViewStyle_Toggle
 ) : BaseCellView(context, attrs, defStyleAttr, defStyleRes) {
 
-    private lateinit var switch: SwitchCompat
+    private var switch: SwitchCompat? = null
+
+    override fun inflateView(context: Context) {
+        super.inflateView(context)
+        inflateSwitch()
+    }
 
     override fun obtainAttributes(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
         super.obtainAttributes(context, attrs, defStyleAttr, defStyleRes)
-        setupViews()
         context.obtainStyledAttributes(attrs, R.styleable.ToggleCellView, defStyleAttr, defStyleRes)
             .run {
                 getString(R.styleable.ToggleCellView_switch_text)?.let { setSwitchText(it) }
@@ -34,44 +38,41 @@ open class ToggleCellView @JvmOverloads constructor(
         }
     }
 
-    private fun setupViews() {
-        addSwitch()
-        setIsChevronVisible(false)
-    }
-
-    private fun addSwitch() {
-        val switch = SwitchCompat(context)
-        switch.tag = "SWITCH"
+    private fun inflateSwitch() {
+        this.switch = SwitchCompat(context)
         view.flEndPlaceholder.addView(switch)
-        this.switch = view.flEndPlaceholder.findViewWithTag(switch.tag)
     }
 
     fun setIsSwitchVisible(isVisible: Boolean) {
-        switch.visibility = when (isVisible) {
+        switch?.visibility = when (isVisible) {
             true -> View.VISIBLE
             else -> View.GONE
         }
     }
 
     fun setIsSwitchChecked(isChecked: Boolean) {
-        switch.isChecked = isChecked
+        switch?.isChecked = isChecked
     }
 
     fun setOnCheckChangeListener(listener: (CompoundButton, Boolean) -> Unit) {
-        switch.setOnCheckedChangeListener(listener)
+        switch?.setOnCheckedChangeListener(listener)
     }
 
     fun setSwitchText(text: String) {
-        switch.text = text
+        switch?.text = text
     }
 
     fun setSwitchTextOn(text: String) {
-        switch.showText = true
-        switch.textOn = text
+        switch?.apply {
+            showText = true
+            textOn = text
+        }
     }
 
     fun setSwitchTextOff(text: String) {
-        switch.showText = true
-        switch.textOff = text
+        switch?.apply {
+            showText = true
+            textOff = text
+        }
     }
 }
