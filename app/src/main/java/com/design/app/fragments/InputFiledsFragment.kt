50b1f1ac4c7bac6bd2d4d22b233e4b8f0b4ff2ec
ealmaz <ealmaz@nurtelecom.kg>
2022-07-29 16:40:52 +0600

    refactor

--- app/src/main/java/com/design/app/fragments/InputFiledsFragment.kt
+++ app/src/main/java/com/design/app/fragments/InputFiledsFragment.kt
@@ -4,15 +4,40 @@ import android.os.Bundle
 import android.view.View
 import com.design.app.base.BaseFragment
 import com.design.app.databinding.FragmentInputFieldsBinding
+import com.design.chili.view.input.PasteListener
 
 class InputFields : BaseFragment<FragmentInputFieldsBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         vb.field1.setupClearTextButton()
+        vb.field0.setPasteTextListener(object : PasteListener {
+            override fun onPasteText(
+                fieldText: String,
+                newText: String,
+                selectionPosition: Int,
+            ): String? {
+                if (newText.length > 9) {
+                    return newText.parsePhoneNumber
+                }
+                return null
+            }
+
+        })
     }
 
     override fun inflateViewBinging(): FragmentInputFieldsBinding {
         return FragmentInputFieldsBinding.inflate(layoutInflater)
     }
 }
+
+val String.parsePhoneNumber: String
+    get() {
+        val number = this.replace("[\\-\\+\\s()]+".toRegex(), "")
+        return when {
+            number.startsWith("996") -> number
+            number.startsWith("+") -> number.replace("+", "")
+            number.startsWith("0") -> "996${number.trimStart('0')}"
+            else -> "996$number"
+        }
+    }
