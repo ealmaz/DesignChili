d332ba4956e669c3f47ccf000c12b867ed2e88ac
ealmaz <ealmaz@nurtelecom.kg>
2022-09-16 16:16:14 +0600

    refactor ui module

--- app/src/main/java/com/design/app/MainActivity.kt
+++ app/src/main/java/com/design/app/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.design.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.*
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.design.app.fragments.MainFragment
@@ -16,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupToolbar()
         if (savedInstanceState == null) {
-            openFragment(MainFragment())
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment())
+                .commit()
         }
     }
 
